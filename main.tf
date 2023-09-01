53b990cbbb9c220faa97ea6f7ec9cd9d8f1d79cf
junpayment <lyric.from.riding.tarsier@gmail.com>
2023-09-01 18:13:50 +0900

    fmt

--- main.tf
+++ main.tf
@@ -13,9 +13,3 @@ resource "null_resource" "thisisnull" {
     command = "echo ${terraform.workspace}"
   }
 }
-
-resource "null_resource" "thisisnull2" {
-  provisioner "local-exec" {
-    command = "echo ${terraform.workspace}_2"
-  }
-}
